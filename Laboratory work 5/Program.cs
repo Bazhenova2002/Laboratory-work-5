0f9291779af5ce46f878344e862b14dc11b43670
annabazenovanka <annabazenovanka@gmail.com>
2019-06-12 11:27:21 +0300

    Good

--- Laboratory work 5/Program.cs
+++ Laboratory work 5/Program.cs
@@ -8,73 +8,67 @@ namespace Class
     interface IBase
     {
         string Name { get; set; }
-        string Author { get; set; }
+        string Surname { get; set; }
     }
 
-    public class Picture : IBase
+    public class Student : IBase
     {
         string name;
-        string author;
+        string surname;
 
         public string Name { get => name; set => name = value; }
-        public string Author { get => author; set => author = value; }
-        public Picture(string name, string author)
+        public string Surname { get => surname; set => surname = value; }
+        public Student(string name, string surname)
         {
             this.Name = name;
-            this.Author = author;
+            this.Surname = surname;
         }
 
         public void PrintName()
         {
-            Console.WriteLine("Name:    " + this.Name + "\nAuthor:   " + this.Author);
+            Console.WriteLine("Name:    " + this.Name + "\nSurname:   " + this.Surname);
         }
     }
 
-    public class Painting : Picture
+    public class Teacher : Student
     {
-        public int Price;
-        public Painting(string name, string author, int price)
-            : base(name, author)
+        public int Age;
+        public Teacher(string name, string surname, int age)
+            : base(name, surname)
         {
-            this.Price = price;
+            this.Age = age;
         }
-        public void PrintPrice()
+        public void PrintAge()
         {
-            Console.WriteLine("Price:    " + this.Price);
+            Console.WriteLine("Age:    " + this.Age);
         }
     }
 
-    public class Remake : Painting
+    public class Persona : Teacher
     {
-        public int Year;
-        public Remake(string name, string author, int price, int year)
-            : base(name, author, price)
+        public string University;
+        public Persona(string name, string surename, int age, string university)
+            : base(name, surename, age)
         {
-            if (year > 0 && year < 2020)
-                this.Year = year;
-            else
-            {
-                Console.WriteLine("Year set to 2019");
-                this.Year = 2019;
-            }
+            this.University = university;
         }
-        public void PrintGodnost()
+        public void PrintUniversity()
         {
-            Console.WriteLine("Year:    " + this.Year);
+            Console.WriteLine("University:    " + this.University);
         }
     }
 
-    public class Landscape : Remake
+    public class HeadofDepartment : Persona
     {
         public string strana;
-        public Landscape(string name, string author, int price, int year, string strana)
-            : base(name, author, price, year)
+        public HeadofDepartment(string name, string surename, int age, string univesity, string strana)
+            : base(name, surename, age, univesity)
         {
             this.strana = strana;
         }
         public void PrintStrana()
         {
-            Console.WriteLine("Place:    " + this.strana);
+            Console.WriteLine("Сountry:    " + this.strana);
         }
     }
 
@@ -83,15 +77,15 @@ namespace Class
         delegate void Output();
         static void Main(string[] args)
         {
-            Picture picture = new Picture("Superman", "Holond");
-            Painting painting = new Painting("The last day of Pompeii", "Karl Brullov", 100000);
-            Remake remake = new Remake("Sunflowers", "Art-Holst", 200, 2019);
-            Landscape landscape = new Landscape("Ninth Shaft", "Ivan Ayvazovskiy", 100000, 1850, "Sea");
+            Student student = new Student("Bill", "Holond");
+            Teacher teacher = new Teacher("Vally", "Kuper", 35);
+            Persona persona = new Persona("Laf", "Molli", 20, "ONPU");
+            HeadofDepartment headofDepartment = new HeadofDepartment("Vinger", "Pepe", 45, "ONPU", "Ukrain");
             Output output;
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("1. Info about PICTURE\n2.Info about PAINTING\n3.Info about REMAKE\n4.Info about LANDSCAPE");
+                Console.WriteLine("1. Info about STUDENT\n2.Info about TEACHER\n3.Info about PERSONA\n4.Info about HEADOFDEPARTMENT");
                 int selection = 0;
                 try
                 {
@@ -106,28 +100,28 @@ namespace Class
                 switch (selection)
                 {
                     case 1:
-                        output = picture.PrintName;
+                        output = student.PrintName;
                         output();
                         Console.ReadKey();
                         break;
                     case 2:
-                        output = painting.PrintName;
-                        output += painting.PrintPrice;
+                        output = teacher.PrintName;
+                        output += teacher.PrintAge;
                         output();
                         Console.ReadKey();
                         break;
                     case 3:
-                        output = remake.PrintName;
-                        output += remake.PrintPrice;
-                        output += remake.PrintGodnost;
+                        output = persona.PrintName;
+                        output += persona.PrintAge;
+                        output += persona.PrintUniversity;
                         output();
                         Console.ReadKey();
                         break;
                     case 4:
-                        output = landscape.PrintName;
-                        output += landscape.PrintPrice;
-                        output += landscape.PrintGodnost;
-                        output += landscape.PrintStrana;
+                        output = headofDepartment.PrintName;
+                        output += headofDepartment.PrintAge;
+                        output += headofDepartment.PrintUniversity;
+                        output += headofDepartment.PrintStrana;
                         output();
                         Console.ReadKey();
                         break;
